195cf901a0cf29a06a22365ea7d21f518c85cc79
Justin <justin.strayhorn@hotmai.com>
2017-02-03 11:17:38 -0600

    created tagline and app title elements, created main video title and details element

--- js/home.js
+++ js/home.js
@@ -6,7 +6,11 @@ export default React.createClass({
 
     return(
       <section className="title__section">
-          <iframe className="movie" width="500" height="315" src="https://www.youtube.com/embed/Lx8FByIE0cw?rel=0" frameBorder="0" allowFullScreen></iframe>
+        <h2 className="tagline"> Great Moments Never Die With </h2>
+        <h1 className="tagline__title"> RETRO PIGSKIN </h1>
+          <iframe className="movie" width="600" height="315" src="https://www.youtube.com/embed/Lx8FByIE0cw?rel=0" frameBorder="0" allowFullScreen></iframe>
+          <h2 className="mainVideo__title"> 2000 NFC Championship Game - Saints vs Rams </h2>
+            <p className="mainVideo__details"> </p>
         <div className="video__carousel">
           <a href="https://youtu.be/pcwsCS9nD3g">
             <img src="/styles/Bucs-Rams2000.jpg" className="video__image"></img>
